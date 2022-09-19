33d2d312216cf8ff3dfdcbd159eb4db4209ecc66
anemoiaa <georgiypetroff@gmail.com>
2022-09-19 23:11:40 +0400

    refactor: minor changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const todoRouter = require('./routes/todo.routes')
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -10,4 +10,4 @@ app.use(express.json());
 app.use('/api', todoRouter);
 
 
-app.listen(PORT, () => console.log(`Server stared on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));

9dd9c980b21c67a7ec0789c4cce5c3eb47b61973
anemoiaa <georgiypetroff@gmail.com>
2022-09-19 19:35:08 +0400

    feat: add todos_status.title to response

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -2,17 +2,17 @@ const db = require('../db');
 
 class TodoController {
     async createTodo(req, res) {
-        const {title, description, status} = req.body;
-        const newTodo = await db.query(
-            `INSERT INTO todo (title, description, status) VALUES ($1, $2, $3) RETURNING * ;`, 
-            [title, description, status]
+        const {title, description, status_id} = req.body;
+        const newTodo = await db.query( 
+            `INSERT INTO todo (title, description, status_id) VALUES ($1, $2, $3) RETURNING *;`,
+            [title, description, status_id]
         );
         res.json(newTodo.rows[0]);  
     }
 
     async getAllTodos(req, res){
         const todos = await db.query(
-            `SELECT * FROM todo;`
+            `SELECT todo.*, todos_status.status_title FROM todo INNER JOIN todos_status ON todo.status_id=todos_status.id;`
         );
         res.json(todos.rows);  
     }
@@ -20,17 +20,17 @@ class TodoController {
     async getOneTodo(req, res) {
         const id = req.params.id;
         const todo = await db.query(
-        `SELECT * FROM todo WHERE id = $1;`,
-        [id]  
+            `SELECT todo.*, todos_status.status_title FROM todo INNER JOIN todos_status ON todo.status_id=todos_status.id where todo.id=$1;`,
+            [id]  
         );
         res.json(todo.rows[0]);
     }
 
     async updateTodo(req, res) {
-        const {id, title, description, status} = req.body;
+        const {id, title, description, status_id} = req.body;
         const todo = await db.query(
-            `UPDATE todo set title = $1, description = $2, status = $3 WHERE id = $4 RETURNING *;`,
-            [title, description, status, id]
+            `UPDATE todo set title = $1, description = $2, status_id = $3 WHERE id = $4 RETURNING *;`,
+            [title, description, status_id, id]
         );
         res.json(todo.rows[0]);
     }
